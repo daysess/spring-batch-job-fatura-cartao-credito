9de79dfa6f7ef82853f21261c5edc2812a34d863
Dayse Soares <daysedesign@gmail.com>
2020-07-17 21:26:21 -0300

    Leitura de banco de dados agrupando transacoes por cliente

--- src/main/java/br/com/daysesoares/faturacartaocredito/step/FaturaCartaoCreditoStepConfig.java
+++ src/main/java/br/com/daysesoares/faturacartaocredito/step/FaturaCartaoCreditoStepConfig.java
@@ -3,13 +3,15 @@ package br.com.daysesoares.faturacartaocredito.step;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.daysesoares.faturacartaocredito.dominio.FaturaCartaoCredito;
+import br.com.daysesoares.faturacartaocredito.dominio.Transacao;
+import br.com.daysesoares.faturacartaocredito.reader.FaturaCartaoCreditoReader;
 
 @Configuration
 public class FaturaCartaoCreditoStepConfig {
@@ -19,14 +21,14 @@ public class FaturaCartaoCreditoStepConfig {
 	
 	@Bean
 	public Step faturaCartaoCreditoStep(
-			ItemReader<FaturaCartaoCredito> lerTransacoesReader,
+			ItemStreamReader<Transacao> lerTransacoesReader,
 			ItemProcessor<FaturaCartaoCredito, FaturaCartaoCredito> carregarDadosClienteProcessor,
 			ItemWriter<FaturaCartaoCredito> escreverFaturaCartaoCredito
 			) {
 		return stepBuilderFactory 
 				.get("faturaCartaoCreditoStep")
 				.<FaturaCartaoCredito, FaturaCartaoCredito> chunk(1)
-				.reader(lerTransacoesReader)
+				.reader(new FaturaCartaoCreditoReader(lerTransacoesReader))
 				.processor(carregarDadosClienteProcessor)
 				.writer(escreverFaturaCartaoCredito)
 				.build();
